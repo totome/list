7b0d0c942a6326cb6b848ff02fce8170de1f9bd7
totome <ttomczyk87@gmail.com>
2014-10-13 16:08:01 +0200

    new changes

--- list.hpp
+++ list.hpp
@@ -1,49 +1,88 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
+
 namespace lst
 {
+  template <typename T>
+  class Elem
+  {
+  public:
+    Elem(T value, Elem<T> * elem = nullptr) : _next_elem(elem)
+    {}
+    
+    ~Elem()
+    {
+      
+    }
+    
+    void setNextElem(Elem * next_elem)
+    {
+	_next_elem = next_elem;
+    }
+    
+    T getValue()
+    {
+      return _value;
+    }
+    
+  private:
+    T _value;
+    Elem<T> * _next_elem;
+  };
+  
   template <typename T>
   class List
   {
   public:
-    List();
-    ~List();
+    List() : head(nullptr), tail(nullptr) 
+    {
+      
+    }
+    
+    ~List()
+    {
+      if(head)
+	delete head;
+      
+      if(tail)
+	delete tail;
+    }
+    
     void push_back(const T &);
-    void push_back(T &&);
+    
+    T pop_back(void);
     
     void push_front(const T &);
-    void push_front(T &&);
     
     void insert(const T &);
-    void insert(T &&);
     
   private:
-    
+    Elem<T> * head;
+    Elem<T> * tail;
   };
   
   template <typename T>
   void List<T>::push_back(const T & value)
   {
-    
-  }
-  
-  template <typename T>
-  void List<T>::push_back(T && rvalue)
-  {
-    
+    auto newtail = new Elem<T>(value);
+    tail -> setNextElem(newtail);
+    tail = newtail;
   }
   
   template <typename T>
   void List<T>::push_front(const T & value)
   {
-    
+    T * tmp = head;
+    head = new Elem<T>(value, head);
   }
   
   template <typename T>
-  void List<T>::push_front(T && rvalue)
+  T List<T>::pop_back()
   {
-    
+    T value = tail->getValue();
+    delete tail;
+    return value;
   }
   
   template <typename T>
@@ -52,23 +91,6 @@ namespace lst
     
   }
   
-  template <typename T>
-  void List<T>::insert(T && rvalue)
-  {
-    
-  }
-  
-  template <typename T>
-  class Elem
-  {
-  public:
-    Elem(T * elem = nullptr) : _next_elem(elem)
-    {}
-    
-    ~Elem();
-  private:
-    T * _next_elem;
-  };
 }
 
 #endif //LIST_HPP
--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <vector>
 #include "myInAndOut.hpp"
+#include "list.hpp"
 
 
 int main(int argc, char **argv)
 {
-  std::vector<int> v;
-  v = myio::read<int>();
-  myio::write<int>(v);
+ // std::vector<double> v;
+ // v = myio::read<double>();
+ // myio::write<double>(v);
+  std::cout<<"!!!!!!!!!!!!!!!!\n";
+  lst::List<double> l;
+  std::cout<<std::endl<<"!!!!!!!!!!!!!!!!\n"<<std::endl;
+  l.push_back(15);
+  std::cout<<std::endl<<"!!!!!!!!!!!!!!!!\n"<<std::endl;
+  std::cout<<"Elelm : "<<l.pop_back()<<std::endl;
   return 0;
 }
